7da2d92ca550e00de1cc8486311ed7f4c6cad8a8
XDdevv <undefineduser087@gmail.com>
2025-10-08 11:26:11 +0500

    Added montserrat, cinzel font family

--- app/src/main/java/zed/rainxch/octoberminichallenges/ui/theme/Type.kt
+++ app/src/main/java/zed/rainxch/octoberminichallenges/ui/theme/Type.kt
@@ -2,9 +2,22 @@ package zed.rainxch.octoberminichallenges.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import zed.rainxch.octoberminichallenges.R
+
+val cinzelFontFamily = FontFamily(
+    Font(R.font.cinzel_regular, FontWeight.Normal),
+    Font(R.font.cinzel_bold, FontWeight.Bold),
+)
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
